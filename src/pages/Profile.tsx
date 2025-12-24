0afa56340a58010e243dc01b090fdf1581349da8
Ekaterina Zaharova <ekaterinazaharova62@gmail.com>
2025-12-25 03:22:46 +0400

    feat: add create post button

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,11 @@
 import {useEffect, useState} from 'react'
 import {me} from '../api/users.ts'
+import Button from "../components/Button/Button.tsx";
+import {useNavigate} from "react-router-dom";
 
 export default function Profile() {
     const [user, setUser] = useState<any>()
+     const navigate = useNavigate()
 
     useEffect(() => {
         me().then(r => setUser(r.data))
@@ -14,6 +17,7 @@ export default function Profile() {
         <div>
             <h2>{user.username}</h2>
             <img src={user.avatar}/>
+            <Button onClick={() => navigate("/create")}>Добавить пост</Button>
         </div>
     )
 }
